7791c46fa8c7c916f36611683b475bcaa9718a47
lmandy <949839367@qq.com>
2018-05-15 15:09:38 +0800

    Initial commit

---
+++ src/main/java/com/bean/UserInfo.java
@@ -0,0 +1,88 @@
+package com.bean;
+
+import javax.persistence.*;
+
+@Table(name = "user_info")
+public class UserInfo {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String address;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return user_id
+     */
+    public Integer getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId
+     */
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
